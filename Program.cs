58db26f7ffa3a5297a035f5702e3546c65c075ca
seoknyang <55523889+seoknyang@users.noreply.github.com>
2020-09-24 17:07:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("첫 번째 정수 입력 :");
+            abc:
+
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("두 번째 정수 입력 :");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + "+" + num2 + "=" + (num1 + num2));
+            Console.WriteLine(num1 + "-" + num2 + "=" + (num1 - num2));
+            Console.WriteLine(num1 + "*" + num2 + "=" + (num1 * num2));
+            Console.WriteLine(num1 + "/" + num2 + "=" + (num1 / num2));
+            goto abc;
+
+        }
+    }
+}
